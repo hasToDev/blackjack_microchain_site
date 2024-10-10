6477cdd36eb634157b403a0036216b944d191dac
Hasto <44645201+hasToDev@users.noreply.github.com>
2024-10-10 11:49:59 +0700

    minor UI update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -14,7 +14,7 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
  _flutter.loader.load({
    serviceWorkerSettings: {
-     serviceWorkerVersion: "2741040071",
+     serviceWorkerVersion: "615500788",
    },
    onEntrypointLoaded: async function(engineInitializer) {
      // initializing engine
