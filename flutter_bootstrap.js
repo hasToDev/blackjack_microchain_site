dd1da6d23817229ee539baf8b72a71b08ab16b13
Hasto <44645201+hasToDev@users.noreply.github.com>
2025-03-14 17:57:32 +0700

    update contract

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -14,7 +14,7 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
  _flutter.loader.load({
    serviceWorkerSettings: {
-     serviceWorkerVersion: "1105903449",
+     serviceWorkerVersion: "3454704106",
    },
    onEntrypointLoaded: async function(engineInitializer) {
      // initializing engine
